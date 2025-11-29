f27ce3659b39d7604dba6e5df84bcc1f87ac942b
Doug Rosman <dougrosman@gmail.com>
2025-11-29 16:03:06 -0600

    Increase number of snowflakes and adjust properties

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let flakeImg;
 let snowflakes = [];
-const NUM_SNOWFLAKES = 1500;
+const NUM_SNOWFLAKES = 4500;
 const bgColor = [0, 0, 0];
 
 function preload() {
@@ -28,33 +28,34 @@ function draw() {
 class Snowflake {
 
     constructor() {
-        this.size = floor(random(3, 10));
-        this.pos = createVector(random(0, width), random(-200000, -200));
+        this.size = floor(random(3, 12));
+        this.pos = createVector(random(0, width), random(-300000, -200));
         this.startX = this.pos.x;
-        this.velY = random(0.2, 0.5);
-        this.accY = random(0.000005, 0.00001);
-        this.radius = random(40, 80);
-        this.rate = random(200, 400); // larger is slower
+        this.velY = random(0.02, 0.05);
+        this.accY = random(0.0000005, 0.000001);
+        this.radius = random(20, 160);
+        this.rate = random(100, 500); // larger is slower
+        this.noiseStep = random(0.01, 0.02);
     }
 
     update() {
         
         //ensure the snow never gets too fast
-        if(this.velY < 1.5) {
+        if(this.velY < 100) {
             this.velY+=this.accY;
         }
         this.pos.x=this.startX + sin(frameCount/this.rate) * this.radius;
         this.pos.y+=this.velY;
         
         if(this.pos.y > height + this.size) {
-            this.pos.y = -20;
+            this.pos.y = -50;
         }
     }
 
     draw() {
         image(flakeImg, this.pos.x, this.pos.y, this.size, this.size);
-        textSize(20);
-        fill(255);
+        //textSize(20);
+        //fill(255);
          
     }
 }
